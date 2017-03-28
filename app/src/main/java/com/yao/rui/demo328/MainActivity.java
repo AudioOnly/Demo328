7678e198617bdab445b9c6b949bbe2351e478ce6
AudioOnly <yao0915ruining>
2017-03-28 18:25:01 +0800

    添加ButterKnife 使用@onClick， 方法不可以为private或static

--- app/src/main/java/com/yao/rui/demo328/MainActivity.java
+++ app/src/main/java/com/yao/rui/demo328/MainActivity.java
@@ -2,7 +2,11 @@ package com.yao.rui.demo328;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.main_tv).setOnClickListener(v -> {
-            Toast.makeText(this,"Lambda使用成功",Toast.LENGTH_SHORT).show();
-        });
+        //初始化ButterKnife
+        ButterKnife.bind(this);
+    }
+    //使用ButterKnife的@onClick，方法不可以为private或static
+    @OnClick(R.id.main_tv)
+    public void OnClick(View v){
+        switch (v.getId()){
+            case R.id.main_tv:
+                Toast.makeText(this,"使用ButterKnife成功",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
     }
 }
